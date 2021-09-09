7b19881e7e09afb978d542d83cb56523d5175867
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-09 16:40:24 +0200

    refactor

--- snack 2/script.js
+++ snack 2/script.js
@@ -6,7 +6,6 @@
 
 
 const randomNumber = () => Math.floor(Math.random() * 20);
-const teamsFaults = [];
 
 // Creare un array di oggetti di squadre di calcio.Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -42,24 +41,26 @@ const teams = [
     'falliSubiti': 0
   }
 ];
+const teamsFaults = [];
 
 // attraverso l'array teams
 for (let i = 0; i < teams.length; i++) {
   
   // Generare numeri random al posto degli 0 nelle proprietà:
   // Punti fatti e falli subiti.
+  let team = teams[i]
   for (let key in teams[i]) {
-    if (teams[i][key] == 0)
-      teams[i][key] = randomNumber();
+    if (team[key] == 0)
+      team[key] = randomNumber();
   }
 
   // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.;
-  const { nome, falliSubiti } = teams[i];
-  const team = {
+  const { nome, falliSubiti } = team;
+  const teamFaults = {
     nome,
     falliSubiti
   }
-  teamsFaults.push(team)
+  teamsFaults.push(teamFaults)
 }
 
 
